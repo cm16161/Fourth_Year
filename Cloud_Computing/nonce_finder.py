eeac76887f6bef342621d702940c69e4840a144e
Chetan Mistry <cmistry98@gmail.com>
2019-10-10 11:18:18 +0100

    Complete POW algorithm
    Signed-off-by: Chetan Mistry <cmistry98@gmail.com>

--- Cloud_Computing/nonce_finder.py
+++ Cloud_Computing/nonce_finder.py
@@ -4,19 +4,20 @@ import hashlib
 def find_nonce(difficulty_level):
     """This function will continually generate nonces and see if they
     are golden"""
-    for i in range(0, 4294967296):
+    for i in range(0, 2**32):
         sha_f = hashlib.sha256()
         num = bytes(i)
         sha_f.update(num)
+        sha_f.update(sha_f.digest())
         result = sha_f.digest()
         bit_shift = 257-difficulty_level
-        #print (int.from_bytes(result, byteorder='little', signed=False))
         if (int.from_bytes(result, byteorder='little', signed=False) >> bit_shift) == 0:
-            print("NONCE")
+            print("Golden Nonce: "+str(i))
 
 
 def main():
     """Main Function"""
     find_nonce(20)
+
 if __name__ == '__main__':
     main()
