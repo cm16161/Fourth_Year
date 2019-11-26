a08320f7a17e069d7aee7f6e33e195c9a4af3243
Chetan Mistry <cmistry98@gmail.com>
2019-11-26 15:30:06 +0000

    <Cloud> Change Script to allow 7 VM's
    Signed-off-by: Chetan Mistry <cmistry98@gmail.com>

--- Cloud_Computing/get_times.py
+++ Cloud_Computing/get_times.py
@@ -1,7 +1,7 @@
 import os
 
 for i in range(21, 33):
-    header = "echo 'Time for D="+str(i) + "' >> times.txt"
+    header = "echo 'Time for N=7 D="+str(i) + "' >> times.txt"
     os.system(header)
-    command = "{ time python generate_in_cloud.py -n 5 -d " +str(i) + " ; } 2>> times.txt"
+    command = "{ time python generate_in_cloud.py -n 7 -d " +str(i) + " ; } 2>> times.txt"
     os.system(command)
