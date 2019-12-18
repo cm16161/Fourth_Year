6e77f5a279f37d1517580c1c483d7799ba7ecc52
Chetan Mistry <cmistry98@gmail.com>
2019-12-18 14:48:33 +0000

    <ADL> Add BC4 script
    Signed-off-by: Chetan Mistry <cmistry98@gmail.com>

---
+++ Applied_Deep_Learning/Assignment/dataset.py
@@ -0,0 +1,57 @@
+import torch
+from torch.utils import data
+import numpy as np
+import pickle
+
+
+class UrbanSound8KDataset(data.Dataset):
+    def __init__(self, dataset_path, mode):
+        self.dataset = pickle.load(open(dataset_path, 'rb'))
+        self.mode = mode
+
+    def __getitem__(self, index):
+        if self.mode == 'LMC':
+            # Edit here to load and concatenate the neccessary features to 
+            # create the LMC feature
+            lms = self.dataset[index]['features']['logmelspec']
+            chroma = self.dataset[index]['features']['chroma']
+            sc = self.dataset[index]['features']['spectral_contrast']
+            tn = self.dataset[index]['features']['tonnetz']
+            
+            feature = np.concatenate((lms,chroma,sc,tn), axis=0)
+            # print(self.dataset.keys())
+            feature = torch.from_numpy(feature.astype(np.float32)).unsqueeze(0)
+        elif self.mode == 'MC':
+            # Edit here to load and concatenate the neccessary features to 
+            # create the MC feature
+            mfcc = self.dataset[index]['features']['mfcc']
+            chroma = self.dataset[index]['features']['chroma']
+            sc = self.dataset[index]['features']['spectral_contrast']
+            tn = self.dataset[index]['features']['tonnetz']
+
+            feature = np.concatenate((mfcc,chroma,sc,tn), axis=0)
+            feature = torch.from_numpy(feature.astype(np.float32)).unsqueeze(0)
+        elif self.mode == 'MLMC':
+
+            mfcc = self.dataset[index]['features']['mfcc']
+            lms = self.dataset[index]['features']['logmelspec']
+            chroma = self.dataset[index]['features']['chroma']
+            sc = self.dataset[index]['features']['spectral_contrast']
+            tn = self.dataset[index]['features']['tonnetz']
+
+            feature = np.concatenate((mfcc,lms,chroma,sc,tn), axis=0)
+
+            # Edit here to load and concatenate the neccessary features to 
+            # create the MLMC feature
+            feature = torch.from_numpy(feature.astype(np.float32)).unsqueeze(0)
+       
+        label = self.dataset[index]['classID']
+        fname = self.dataset[index]['filename']
+        print(feature.size())
+        return feature, label, fname
+
+    def __len__(self):
+        return len(self.dataset)
+
+x = UrbanSound8KDataset('UrbanSound8K_test.pkl', 'MLMC')
+print(x.__getitem__(0))
---
+++ Applied_Deep_Learning/Assignment/train_cifar.py
@@ -0,0 +1,368 @@
+#!/usr/bin/env python3
+import time
+from multiprocessing import cpu_count
+from typing import Union, NamedTuple
+from dataset import UrbanSound8KDataset
+import torch
+import torch.backends.cudnn
+import numpy as np
+from torch import nn, optim
+from torch.nn import functional as F
+import torchvision.datasets
+from torch.optim.optimizer import Optimizer
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms
+import sys; 
+import argparse
+from pathlib import Path
+
+torch.backends.cudnn.benchmark = True
+
+parser = argparse.ArgumentParser(
+    description="Train a simple CNN on audio",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+default_dataset_dir = Path.home() / ".cache" / "torch" / "datasets"
+parser.add_argument("--dataset-root", default=default_dataset_dir)
+parser.add_argument("--log-dir", default=Path("logs"), type=Path)
+parser.add_argument("--learning-rate", default=1e-1, type=float, help="Learning rate")
+parser.add_argument(
+    "--batch-size",
+    default=128,
+    type=int,
+    help="Number of images within each mini-batch",
+)
+parser.add_argument(
+    "--epochs",
+    default=20,
+    type=int,
+    help="Number of epochs (passes through the entire dataset) to train for",
+)
+parser.add_argument(
+    "--audiotype",
+    default="LMC",
+    type=str,
+    help="The audio type coming from the dataset loader (tonnetz, LMC...)",
+)
+parser.add_argument(
+    "--val-frequency",
+    default=2,
+    type=int,
+    help="How frequently to test the model on the validation set in number of epochs",
+)
+parser.add_argument(
+    "--log-frequency",
+    default=10,
+    type=int,
+    help="How frequently to save logs to tensorboard in number of steps",
+)
+parser.add_argument(
+    "--print-frequency",
+    default=10,
+    type=int,
+    help="How frequently to print progress to the command line in number of steps",
+)
+parser.add_argument(
+    "-j",
+    "--worker-count",
+    default=cpu_count(),
+    type=int,
+    help="Number of worker processes used to load data.",
+)
+
+
+class ImageShape(NamedTuple):
+    height: int
+    width: int
+    channels: int
+
+
+if torch.cuda.is_available():
+    DEVICE = torch.device("cuda")
+else:
+    DEVICE = torch.device("cpu")
+
+
+def main(args):
+    transform = transforms.ToTensor()
+   
+    train_loader = torch.utils.data.DataLoader( 
+      UrbanSound8KDataset("UrbanSound8K_train.pkl", args.audiotype), 
+      batch_size=32, shuffle=True, 
+      num_workers=8, pin_memory=True
+    ) 
+
+    test_loader = torch.utils.data.DataLoader( 
+     UrbanSound8KDataset("UrbanSound8K_test.pkl", args.audiotype), 
+     batch_size=32, shuffle=False, 
+     num_workers=8, pin_memory=True
+    )
+
+    model = CNN(height=len(train_loader), width=32, channels=1, class_count=10)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=args.learning_rate, momentum=0.9)
+
+    log_dir = get_summary_writer_log_dir(args)
+    print(f"Writing logs to {log_dir}")
+    summary_writer = SummaryWriter(
+            str(log_dir),
+            flush_secs=5
+    )
+    trainer = Trainer(
+        model, train_loader, test_loader, criterion, optimizer, summary_writer, DEVICE
+    )
+
+    trainer.train(
+        args.epochs,
+        args.val_frequency,
+        print_frequency=args.print_frequency,
+        log_frequency=args.log_frequency
+    )
+
+    summary_writer.close()
+
+
+class CNN(nn.Module):
+    def __init__(self, height: int, width: int, channels: int, class_count: int):
+        super().__init__()
+        self.input_shape = ImageShape(height=height, width=width, channels=channels)
+        self.class_count = class_count
+
+        self.conv1 = nn.Conv2d(
+            in_channels=self.input_shape.channels,
+            out_channels=32,
+            kernel_size=(3, 3),
+            stride=(2, 2),
+            padding=(2, 2)
+        )
+
+        self.conv12 = nn.Conv2d(
+            in_channels=32,
+            out_channels=32,
+            kernel_size=(3, 3),
+            padding=(1, 1)
+        )
+
+        self.initialise_layer(self.conv1)
+        self.initialise_layer(self.conv12)
+
+        self.pool1 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        self.pool2 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+
+        self.conv2 = nn.Conv2d(
+            in_channels=32,
+            out_channels=64,
+            kernel_size=(3, 3),
+            padding=(1, 1)
+        )
+
+        self.conv22 = nn.Conv2d(
+            in_channels=64,
+            out_channels=64,
+            kernel_size=(3, 3),
+            padding=(1, 1)
+        )
+
+        self.batchNorm2D_0 = nn.BatchNorm2d(
+            num_features = 32
+        )
+
+        self.batchNorm2D_1 = nn.BatchNorm2d(
+            num_features = 64
+        )
+        
+        self.initialise_layer(self.conv2)
+        self.initialise_layer(self.conv22)
+
+        #Pooling layer here
+        
+        self.fc1 = nn.Linear(3520, 1024)
+        self.initialise_layer(self.fc1)
+        self.fc2 = nn.Linear(1024, 10)
+        self.initialise_layer(self.fc2)
+        
+        self.fc12 = nn.Linear(15488, 1024)
+        self.initialise_layer(self.fc12)
+
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, images: torch.Tensor) -> torch.Tensor:
+        x = F.relu(self.batchNorm2D_0(self.conv1(images)))
+        print(images.shape)
+        #x = (self.conv1(images))
+        #x = self.pool1(x)
+        x = F.relu(self.batchNorm2D_0(self.conv12(x)))
+        #print(x.shape)
+        x = self.pool1(x)
+        #print(x.shape)
+        x = F.relu(self.batchNorm2D_1(self.conv2(x)))
+        #print(x.shape)
+        x = F.relu(self.batchNorm2D_1(self.conv22(x)))
+        #print(x.shape)
+        x = self.pool1(x)
+        x = torch.flatten(x,1)
+        #print(x.shape)
+        x = self.fc1(x)
+        #print(x.shape)
+        s = nn.Sigmoid()
+        x = s(x)
+        x = self.fc2(x)
+        
+        return x
+
+    @staticmethod
+    def initialise_layer(layer):
+        if hasattr(layer, "bias"):
+            nn.init.zeros_(layer.bias)
+        if hasattr(layer, "weight"):
+            nn.init.kaiming_normal_(layer.weight)
+
+
+class Trainer:
+    def __init__(
+        self,
+        model: nn.Module,
+        train_loader: DataLoader,
+        val_loader: DataLoader,
+        criterion: nn.Module,
+        optimizer: Optimizer,
+        summary_writer: SummaryWriter,
+        device: torch.device,
+    ):
+        self.model = model.to(device)
+        self.device = device
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.summary_writer = summary_writer
+        self.step = 0
+
+    def train(
+        self,
+        epochs: int,
+        val_frequency: int,
+        print_frequency: int = 20,
+        log_frequency: int = 5,
+        start_epoch: int = 0
+    ):
+        self.model.train()
+        for epoch in range(start_epoch, epochs):
+            self.model.train()
+            data_load_start_time = time.time()
+            for i, (batch, labels, filename) in enumerate(self.train_loader): 
+                batch = batch.to(self.device)
+                labels = labels.to(self.device)
+                data_load_end_time = time.time()
+                logits = self.model.forward(batch)
+                loss = self.criterion(logits, labels)
+                loss.backward()
+                self.optimizer.step()
+                self.optimizer.zero_grad()
+
+                with torch.no_grad():
+                    preds = logits.argmax(-1)
+                    accuracy = compute_accuracy(labels, preds)
+
+                data_load_time = data_load_end_time - data_load_start_time
+                step_time = time.time() - data_load_end_time
+                if ((self.step + 1) % log_frequency) == 0:
+                    self.log_metrics(epoch, accuracy, loss, data_load_time, step_time)
+                if ((self.step + 1) % print_frequency) == 0:
+                    self.print_metrics(epoch, accuracy, loss, data_load_time, step_time)
+
+                self.step += 1
+                data_load_start_time = time.time()
+
+            self.summary_writer.add_scalar("epoch", epoch, self.step)
+            if ((epoch + 1) % val_frequency) == 0:
+                self.validate()
+                self.model.train()
+
+    def print_metrics(self, epoch, accuracy, loss, data_load_time, step_time):
+        epoch_step = self.step % len(self.train_loader)
+        print(
+                f"epoch: [{epoch}], "
+                f"step: [{epoch_step}/{len(self.train_loader)}], "
+                f"batch loss: {loss:.5f}, "
+                f"batch accuracy: {accuracy * 100:2.2f}, "
+                f"data load time: "
+                f"{data_load_time:.5f}, "
+                f"step time: {step_time:.5f}"
+        )
+
+    def log_metrics(self, epoch, accuracy, loss, data_load_time, step_time):
+        self.summary_writer.add_scalar("epoch", epoch, self.step)
+        self.summary_writer.add_scalars(
+                "accuracy",
+                {"train": accuracy},
+                self.step
+        )
+        self.summary_writer.add_scalars(
+                "loss",
+                {"train": float(loss.item())},
+                self.step
+        )
+        self.summary_writer.add_scalar(
+                "time/data", data_load_time, self.step
+        )
+        self.summary_writer.add_scalar(
+                "time/data", step_time, self.step
+        )
+
+    def validate(self):
+        results = {"preds": [], "labels": []}
+        total_loss = 0
+        self.model.eval()
+
+        with torch.no_grad():
+            print(len(self.val_loader))
+            for i, (batch, labels, filename) in enumerate(self.val_loader):
+                batch = batch.to(self.device)
+                labels = labels.to(self.device)
+                logits = self.model(batch)
+                loss = self.criterion(logits, labels)
+                total_loss += loss.item()
+                preds = logits.argmax(dim=-1).cpu().numpy()
+                results["preds"].extend(list(preds))
+                results["labels"].extend(list(labels.cpu().numpy()))
+
+        accuracy = compute_accuracy(
+            np.array(results["labels"]), np.array(results["preds"])
+        )
+        average_loss = total_loss / len(self.val_loader)
+
+        self.summary_writer.add_scalars(
+                "accuracy",
+                {"test": accuracy},
+                self.step
+        )
+        self.summary_writer.add_scalars(
+                "loss",
+                {"test": average_loss},
+                self.step
+        )
+        print(f"validation loss: {average_loss:.5f}, accuracy: {accuracy * 100:2.2f}")
+
+
+def compute_accuracy(
+    labels: Union[torch.Tensor, np.ndarray], preds: Union[torch.Tensor, np.ndarray]
+) -> float:
+    assert len(labels) == len(preds)
+    return float((labels == preds).sum()) / len(labels)
+
+
+def get_summary_writer_log_dir(args: argparse.Namespace) -> str:
+    tb_log_dir_prefix = f'CNN_bn_bs={args.batch_size}_lr={args.learning_rate}_momentum=0.9_run_'
+    i = 0
+    while i < 1000:
+        tb_log_dir = args.log_dir / (tb_log_dir_prefix + str(i))
+        if not tb_log_dir.exists():
+            return str(tb_log_dir)
+        i += 1
+    return str(tb_log_dir)
+
+
+if __name__ == "__main__":
+    main(parser.parse_args())
